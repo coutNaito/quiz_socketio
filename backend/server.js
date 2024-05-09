1915b91835818a157ee3510253947447472c3af1
Ekaruch Purathananont <73429510+coutNaito@users.noreply.github.com>
2024-05-09 19:06:10 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 const bodyParser = require("body-parser");
 const { Server } = require("socket.io");
-const mongoose = require("mongoose");
 const express = require("express");
 const dotenv = require("dotenv");
 const http = require("http");
@@ -21,65 +20,32 @@ server.use(cors());
 const port = 4000;
 const httpServer = http.createServer(server);
 
-//Connect to MongoDB
-async function main() {
-  await mongoose.connect(`mongodb://127.0.0.1:27017/${process.env.DB_Name}`);
-
-  const QuizModel = require("./models/quiz.model");
-  const RoomModel = require("./models/room.model");
-
-  server.get("/", (req, res) => {
-    res.status(200).send("connected backend successfully!");
-  });
-
-  const verifyFirebaseToken = require("./middlewares/auth.middleware");
-  const quizRouter = require("./routes/quiz.route");
-  const roomRouter = require("./routes/room.route");
-
-  server.use("/api/quizzes", verifyFirebaseToken, quizRouter);
-  server.use("/api/rooms", roomRouter);
-}
-
-main().catch((err) => console.log(err));
-
 const io = new Server(httpServer, {
   cors: {
     origin: "*", // Allow requests from any origin, you can specify your frontend URL here
-    methods: ["GET", "POST"], // Allow only GET and POST requests
+    methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
   },
 });
-
-///////////////////
-
-//Connect to firebase
-
-//Declare variable for quiz session
-const { QuizModel, RelatedQuizModel } = require("./models/quiz.model.js");
 
 // For quiz session
 io.on("connection", (socket) => {
   console.log("user : " + socket.id + " connected");
 
-  socket.on("create_room", async (user_id, room_id) => {
+  socket.on("create_room", (room_id) => {
     //Function query username
     console.log("user : " + socket.id + " create room ");
-    //Read Room data from database
-    const quizzes = await QuizModel.findOne({}).lean();
-    const room = await RoomModel.findOne({
-      room_id: room_id,
-      user_id: user_id,
-      quiz_id: quizzes._id,
+    //Read Room data from API
+    //Call at localhost:3000/
+    server.patch("http://localhost:3000/" + room_id, (req, res) => {
+      console.log("room : " + room_id + " data : " + res);
     });
     //////////////////////////////
-    //temp variable after read room
-    var room_socket = 1234;
-    //join room (socket)
-    socket.join(room_socket);
-    console.log("user : " + socket.id + " joined room : " + room_socket);
+    socket.join(room_id);
+    console.log("user : " + socket.id + " joined room : " + room_id);
 
     socket.broadcast
-      .to(room_socket)
-      .emit("new-message", room_socket.toString());
+      .to(room_id)
+      .emit("user : " + socket.id + " joined room : " + room_id);
   });
 
   // When a client disconnects
